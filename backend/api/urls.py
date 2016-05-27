1f75f6a522b97e665cbf58db8afc5fdd43df5727
Your Name <sergey_umnov@mail.ru>
2016-05-27 22:32:20 +0300

    Create initial urls file

---
+++ backend/api/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from api import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
