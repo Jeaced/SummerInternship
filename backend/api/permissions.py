3a29f966cdedd10920d68f17add22c5c72ca33d6
Your Name <sergey_umnov@mail.ru>
2016-05-27 17:29:09 +0300

    Add permissions

---
+++ backend/api/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions 
+
+SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
+
+###########################################################################################
+# You can also use IsAuthenticated and IsAdminUser(actually admin or manager) permissions #
+# from rest_framework.permissions 														  #	
+###########################################################################################
+
+class IsManagerOrReadOnly(permissions.BasePermission):
+	def has_permission(self, request, view):
+		return (
+			(request.user and request.user.is_staff) or
+			(request.user and request.user.is_authenticated() and request.method in SAFE_METHODS)
+			)
+
+class IsSuperuser(permissions.BasePermission):
+	def has_permission(self, request, view):
+		return (request.user and request.user.is_superuser)
+			
