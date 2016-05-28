7a9dbeaec73a90baee8a73d9f5583fa3c997263c
Your Name <sergey_umnov@mail.ru>
2016-05-28 11:55:26 +0300

    Add serialization of single item object

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
 	url(r'^items/$', views.ItemList.as_view()),
+	url(r'^items/(?P<pk>[0-9]+)/$', views.ItemDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,6 +6,7 @@ from api.permissions import IsManagerOrReadOnly, IsSuperuser
 from api.serializers import ItemSerializer
 from api.models import Item
 from rest_framework import status, permissions
+from django.http import Http404
 
 # Create your views here.
 class ItemList(APIView):
@@ -22,3 +23,30 @@ class ItemList(APIView):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ItemDetail(APIView):
+	permission_classes = (IsManagerOrReadOnly,)
+
+	def get_object(self, pk):
+		try:
+			return Item.objects.get(pk=pk)
+		except Item.DoesNotExist:
+			raise Http404
+
+	def get(self, request, pk, format=None):
+		item = self.get_object(pk)
+		serializer = ItemSerializer(item)
+		return Response(serializer.data)
+
+	def put(self, request, pk, format=None):
+		item = self.get_object(pk)
+		serializer = ItemSerializer(dish, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)	
+
+	def delete(self, request, pk, format=None):
+		item = self.get_object(pk)
+		item.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)		
